ef1047f0cc9e7dc0f4237abc1efb372b789ace23
Khalid B.S <khalid_bs@hotmail.com>
2017-10-07 17:22:16 +0200

    Getting rid of the apple

--- CleanSnake/Display.cs
+++ CleanSnake/Display.cs
@@ -45,8 +45,7 @@ namespace CleanSnake {
         {
             Console.ForegroundColor = ConsoleColor.Green; 
             Console.SetCursorPosition(apple.X, apple.Y);
-            Console.Write("$");
-
+            
         }
 
         public bool IsOutside (Vector2D v) => (v.X < 0 || v.X > Width || v.Y < 0 || v.Y > Height);
