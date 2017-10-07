f2e98f15ed5f9d1e454cb272c95520f81ab00786
Otard95 <stian.myklebostad@getmail.no>
2017-10-07 14:30:39 +0200

    apple Constructor edit

--- CleanSnake/Apple.cs
+++ CleanSnake/Apple.cs
@@ -5,46 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanSnake {
-    class Apple : Vector2D {
+	class Apple : Vector2D {
 
-        public Apple (List<Vector2D> mask, int x = 0, int y = 0): base(x,y) { // use initilize the apples pos somewhere not on the snake
-          
-          if (checkIsOutside(mask))
-          {
-            X = x;
-            Y = y;
-          }
-    }
+		public Apple (int xMax, int yMax, List<Vector2D> mask, int x = 0, int y = 0) : base(x, y) { // use initilize the apples pos somewhere not on the snake
+			ChangePos(xMax, yMax, mask);
+		}
 
-        public void ChangePos(int xMax, int yMax, List<Vector2D> mask = null){
-          Random rnd = new Random();
-          bool okPos = false;
-          int x;
-          int y;
-          while (!okPos)
-          {
-            x = rnd.Next(0, xMax);
-            y = rnd.Next(0, yMax);
-            if (checkIsOutside(mask))
-            {
-              X = x;
-              Y = y;
-              okPos = true;
-            }
-          }
-    }
+		public void ChangePos (int xMax, int yMax, List<Vector2D> mask = null) {
+			Random rnd = new Random();
+			bool okPos = false;
+			int x;
+			int y;
+			while (!okPos) {
+				x = rnd.Next(0, xMax);
+				y = rnd.Next(0, yMax);
+				if (checkIsOutside(mask)) {
+					X = x;
+					Y = y;
+					okPos = true;
+				}
+			}
+		}
 
-      public bool checkIsOutside(List<Vector2D> mask)
-      {
-        bool isOutside = true;
-      foreach (Vector2D v in mask)
-      {
-        if (this == v) {
-          isOutside = false;
-        }
-      }
-        return isOutside;
-      }
+		public bool checkIsOutside (List<Vector2D> mask) {
+			bool isOutside = true;
+			foreach (Vector2D v in mask) {
+				if (this == v) {
+					isOutside = false;
+				}
+			}
+			return isOutside;
+		}
 
-    }
+	}
 }
