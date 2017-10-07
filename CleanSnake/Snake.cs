7df25c4b750c0df798eb280637396a9897f11d06
slafa/GK <gklinden@gmail.com>
2017-10-07 17:27:25 +0200

    filled methods

--- CleanSnake/Snake.cs
+++ CleanSnake/Snake.cs
@@ -11,9 +11,8 @@ namespace CleanSnake {
 		public Vector2D LastDir; // movement direction of snake
 
 		public Snake (Vector2D startDir) { // Initial movement dir of snake
-      parts = new List<BodyPart>();
 			// Create snake
-
+      parts = new List<BodyPart>();
 		}
 
 		public BodyPart GetNewHead () {
@@ -25,16 +24,21 @@ namespace CleanSnake {
 		}
 
 		public void UpdateHead (BodyPart newHead) { // add the new head to the parts List
-
+      parts.Add(GetNewHead());
 		}
 
 		public List<Vector2D> BodyToVector2D () { // makes new list ov Vector2D and populates it by converting each BodyPart to a vector
-
+		  var vectorList = new List<Vector2D>();
+		  foreach (var variable in parts)
+		  {
+		    vectorList.Add(variable);
+		  }
+		  return vectorList;
 		}
 
-	  public void RemoveTail()
+    public void RemoveTail()
 	  {
-	    
+	    parts.Remove(parts[0]);
 	  }
 
 
