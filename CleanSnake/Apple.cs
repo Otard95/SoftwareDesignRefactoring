4a5404b609c229fcb100866b1d03d9070334e749
Otard95 <stian.myklebostad@getmail.no>
2017-10-07 17:19:35 +0200

    added icon

--- CleanSnake/Apple.cs
+++ CleanSnake/Apple.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace CleanSnake {
 	class Apple : Vector2D {
 
+		public char Icon = '$';
+
 		public Apple (int xMax, int yMax, List<Vector2D> mask, int x = 0, int y = 0) : base(x, y) { // initilize the apples pos somewhere not on the snake
 			ChangePos(xMax, yMax, mask);
 		}
