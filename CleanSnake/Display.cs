5209ace942def4ecb92a0914c71863e24aaf5621
Khalid B.S <khalid_bs@hotmail.com>
2017-10-07 12:52:07 +0200

    Update on Display

--- CleanSnake/Display.cs
+++ CleanSnake/Display.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace CleanSnake {
     class Display {
 
-        public int Height;
-        public int Width;
+        public int Height { get; set;}
+        public int Width { get; set; }
 
         public Display () {
             Height = Console.WindowHeight; Width = Console.WindowWidth;
@@ -26,8 +26,15 @@ namespace CleanSnake {
 
         }
 
-        public bool IsOutside(Vector2D v) {
+        public bool IsOutside(Vector2D v)
+        {
+            //I understand that this method is to find out the fact if the snake is to big for the Terminal Window. -kbsa00 
+            if (v.X == Width && v.Y == Height)
+            {
+                return true; 
+            }
 
+            return false; 
         }
 
     }
