cb7ef4b1ee522a589a54437f92f6fe7378d2d946
slafa/GK <gklinden@gmail.com>
2017-10-07 13:37:45 +0200

    changes apple

--- CleanSnake/Apple.cs
+++ CleanSnake/Apple.cs
@@ -8,12 +8,35 @@ namespace CleanSnake {
     class Apple : Vector2D {
 
         public Apple (List<Vector2D> mask, int x = 0, int y = 0): base(x,y) { // use initilize the apples pos somewhere not on the snake
-
+          bool isOutside = true;
+        for (int i = 0; i < mask.Count; i++) {
+        if (this == mask[i])
+        {
+          isOutside = false;
         }
+      }
+          if (isOutside)
+          {
+            X = x;
+            Y = y;
+          }
+    }
 
-        public void ChangePos(List<Vector2D> mask = null) {
-
-        }
+        public void ChangePos(int xMax, int yMax, List<Vector2D> mask = null){
+          Random rnd = new Random();
+          bool okPos = false;
+          int x;
+          int y;
+          while (!okPos)
+          {
+            x = rnd.Next(0, xMax);
+            y = rnd.Next(0, yMax);
+            for (int i = 0; i < mask.count; i++)
+            {
+              
+            }
+          }
+    }
 
     }
 }
