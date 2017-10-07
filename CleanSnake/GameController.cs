1f0e96a2e384095e9cdff87b84d9172dbc8970ad
Otard95 <stian.myklebostad@getmail.no>
2017-10-07 12:29:34 +0200

    Constructor all done

--- CleanSnake/GameController.cs
+++ CleanSnake/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace CleanSnake {
     class GameController {
@@ -10,12 +10,38 @@ namespace CleanSnake {
         private Snake snake;
         private Apple apple;
         private InputHandler IH;
+        private Thread IHThread;
         private Display display;
+        private bool playing, paused;
+        private short ticksPerSecond;
 
         public GameController () {
-            snake = new Snake();
-            apple = new Apple();
+
+            // Setup primitives
+            playing = true;
+            paused = false;
+            ticksPerSecond = 10;
+
+            // Setup the snake and apple
+            snake = new Snake(new Vector2D(1, 0));
+            apple = new Apple(snake.BodyToVector2D());
+
+            // Initilize the input handler
+            IH = new InputHandler();
+            IHThread = new Thread(IH.Run);
+            IHThread.Start();
+            // make sure thread is alive and running
+            while (!IHThread.IsAlive) ;
+            Thread.Sleep(1);
+
+            // now last but not least lets set up the display
+            display = new Display();
+            display.PaintSnake(snake.parts);
+            display.PaintApple(apple);
+            
         }
+
+
 
     }
 }
