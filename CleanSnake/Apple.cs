7efaab56f6ebb1a97f3cf997545c7c4ad72b21cc
Otard95 <stian.myklebostad@getmail.no>
2017-10-12 13:49:24 +0200

    apple can't spawn on snakes new head

--- CleanSnake/Apple.cs
+++ CleanSnake/Apple.cs
@@ -11,26 +11,26 @@ namespace CleanSnake {
 		private Random rnd;
 
 		public Apple (int xMax, int yMax, List<Vector2D> mask, int x = 0, int y = 0) : base(x, y) { // initilize the apples pos somewhere not on the snake
-			ChangePos(xMax, yMax, mask);
 			Icon = '$';
 			rnd = new Random();
+			ChangePos(xMax, yMax, mask);
 		}
 
-		public void ChangePos (int xMax, int yMax, List<Vector2D> mask = null) { // !!#!#!# MUST FIX !!#!#!#!#!#!#
+		public void ChangePos (int xMax, int yMax, Vector2D nHead, List<Vector2D> mask = null) {
 			bool okPos = false;
 			while (!okPos) {
 				X = rnd.Next(0, xMax);
 				Y = rnd.Next(0, yMax);
-				if (checkIsOutside(mask)) {
+				if (checkIsOutside(mask, nHead)) {
 					okPos = true;
 				}
 			}
 		}
 
-		private bool checkIsOutside (List<Vector2D> mask) {
+		private bool checkIsOutside (List<Vector2D> mask, Vector2D nHead) {
 			bool isOutside = true;
 			foreach (Vector2D v in mask) {
-				if (this == v) {
+				if (this == v || nHead == v) {
 					isOutside = false;
 				  break;
 				}
